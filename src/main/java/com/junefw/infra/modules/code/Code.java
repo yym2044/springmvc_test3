50d2ecaadca187c270ddc0a329a147adcc87f983
yym2044 <yym2044@gmail.com>
2022-03-02 12:38:35 +0900

    .

---
+++ src/main/java/com/junefw/infra/modules/code/Code.java
@@ -0,0 +1,31 @@
+package com.junefw.infra.modules.code;
+
+public class Code {
+	
+//	DTO
+	private String ifcgSeq;
+	private String ifcgName;
+	private Integer ifcgDelNy;
+	
+//------------
+	public String getIfcgSeq() {
+		return ifcgSeq;
+	}
+	public void setIfcgSeq(String ifcgSeq) {
+		this.ifcgSeq = ifcgSeq;
+	}
+	public String getIfcgName() {
+		return ifcgName;
+	}
+	public void setIfcgName(String ifcgName) {
+		this.ifcgName = ifcgName;
+	}
+	public Integer getIfcgDelNy() {
+		return ifcgDelNy;
+	}
+	public void setIfcgDelNy(Integer ifcgDelNy) {
+		this.ifcgDelNy = ifcgDelNy;
+	}
+
+	
+}
---
+++ src/main/java/com/junefw/infra/modules/code/CodeService.java
@@ -0,0 +1,9 @@
+package com.junefw.infra.modules.code;
+
+import java.util.List;
+
+public interface CodeService {
+	
+	public List<Code> selectList() throws Exception; 
+	
+}
